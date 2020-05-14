fc9df18ff6e600abd144fdba42f12a67a8403b30
Jimmy Lee <jimmylee@miflydesign.info>
2020-05-14 18:36:44 +0800

    合併程式碼

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,36 @@
-let login = document.getElementById('login');
-let share = document.getElementById('share');
+switch (location.pathname) {
+  case '/':
+  case '/index':
+  case '/index.html':
+    let fbLoginButton = document.getElementById('fb-login-button');
+    let fbShareButton = document.getElementById('fb-share-button');
 
-login.addEventListener('click', fbLogin, false);
+    fbLoginButton.addEventListener('click', fbLogin, false);
 
-share.addEventListener('click', fbShare, false);
+    fbShareButton.addEventListener('click', fbShare, false);
+
+    break;
+  default:
+    let logout = document.getElementById('logout');
+
+    let checkWhichIsLoggedIn = function() {
+      if (isFbLoggedIn && !isGoogleLoggedIn) {
+        fbRevokePermission();
+      }
+      else if (!isFbLoggedIn && isGoogleLoggedIn) {
+        googleRevokeAllScopes();
+      }
+      else if (!isFbLoggedIn && !isGoogleLoggedIn) {
+        // 都沒登入，應該要另外處理
+        alert('請先登入');
+        location.replace(`${location.protocol}//${location.host}/`);
+      }
+      else {
+        alert('帳號重複登入');
+      }
+    };
+
+    logout.addEventListener('click', checkWhichIsLoggedIn, false);
+
+    break;
+}
